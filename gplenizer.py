6390451910ad36c2d6fe11e40722071ee1aae3b8
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-12-24 20:10:07 +0100

    Added the gplenizer script and prefix.txt, as I wrote them on May 20th of 2013.

---
+++ gplenizer.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+
+import os
+
+SOURCE_DIRECTORY = r"C:\development\workspaces\eclipse-4.2\umvc3-replay-analyser\umvc3replayanalyser\src"
+
+def main():
+    '''
+    Adds the contentx of prefix.txt to the start of all .java files in SOURCE_DIRECTORY.
+    '''
+    text = open('prefix.txt', 'r').read()
+    
+    for path, directories, files in os.walk(SOURCE_DIRECTORY):
+        for filename in files:
+            if filename.endswith(".java"):
+                fullPath = path + os.sep + filename
+                print fullPath
+                
+                f = open(fullPath, "r+")
+                lines = f.readlines() # read old content
+                f.seek(0) # go back to the beginning of the file
+                f.write(text) # write new content at the beginning
+                for line in lines: # write old content after new
+                   f.write(line)
+                f.close()
+
+if (__name__ == "__main__"):
+    main()
